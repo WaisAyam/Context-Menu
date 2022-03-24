f5c1efb2f861da32b12789f759fd66379c1aa588
Wais <89729829+WaisAyam@users.noreply.github.com>
2022-03-24 15:51:28 +0500

    Update script.js

--- Context Menu/script.js
+++ Context Menu/script.js
@@ -1,43 +1,39 @@
-const context_Menu = document.querySelector('.wrapper');//1
-const share_menu = context_Menu.querySelector('.share-menu');//5
-const share_menu_one = context_Menu.querySelector('.share-menu-one');//5.1
-const share_menu_two = context_Menu.querySelector('.share-menu-two');//5.1
+const context_Menu = document.querySelector('.wrapper');
+const share_menu = context_Menu.querySelector('.share-menu');
+const share_menu_one = context_Menu.querySelector('.share-menu-one');
+const share_menu_two = context_Menu.querySelector('.share-menu-two');
 
-document.addEventListener('contextmenu', (e) => { //1
-    // console.log(e);//1
-    e.preventDefault();//1.1
-    context_Menu.style.visibility = 'visible';//1.1
+document.addEventListener('contextmenu', (e) => { 
+    e.preventDefault();
+    context_Menu.style.visibility = 'visible';
 
-    let x = e.offsetX;//2
-    let y = e.offsetY;//2
-    // console.log(x, y);//2
-    context_Menu.style.left = x + 'px';//3
-    context_Menu.style.top = y + 'px';//3
+    let x = e.offsetX;
+    let y = e.offsetY;
+    context_Menu.style.left = x + 'px';
+    context_Menu.style.top = y + 'px';
 
-    let window_width = window.innerWidth;//4
-    let window_height = window.innerHeight;//4
-    // console.log( window_width, window_height)//4
-    let context_Menu_width = context_Menu.offsetWidth;//4
-    let context_Menu_height = context_Menu.offsetHeight;//4
-    // console.log(context_Menu_width, context_Menu_height)//4
+    let window_width = window.innerWidth;
+    let window_height = window.innerHeight;
+    let context_Menu_width = context_Menu.offsetWidth;
+    let context_Menu_height = context_Menu.offsetHeight;
 
-    if (x > (window_width - context_Menu_width - share_menu.offsetWidth)) { //5
-        share_menu.style.left = '-200px';//5
-        share_menu_one.style.left = '-200px';//5.1
-        share_menu_two.style.left = '-200px';//5.1
+    if (x > (window_width - context_Menu_width - share_menu.offsetWidth)) { 
+        share_menu.style.left = '-200px';
+        share_menu_one.style.left = '-200px';
+        share_menu_two.style.left = '-200px';
     }else{
-        share_menu.style.left = '';//5
-        share_menu.style.right = '-200px';//5
-        share_menu_one.style.left = '';//5.1
-        share_menu_one.style.right = '-200px';//5.1
-        share_menu_two.style.left = '';//5.1
-        share_menu_two.style.right = '-200px';//5.1
+        share_menu.style.left = '';
+        share_menu.style.right = '-200px';
+        share_menu_one.style.left = '';
+        share_menu_one.style.right = '-200px';
+        share_menu_two.style.left = '';
+        share_menu_two.style.right = '-200px';
     }
 
-    x = x > window_width - context_Menu_width ? window_width - context_Menu_width: x; //6
-    y = y > window_height - context_Menu_height ? window_height - context_Menu_height: y;//6
+    x = x > window_width - context_Menu_width ? window_width - context_Menu_width: x; 
+    y = y > window_height - context_Menu_height ? window_height - context_Menu_height: y;
 });
 
-document.addEventListener('click', () => {//1.2
-    context_Menu.style.visibility = 'hidden';//1.2
+document.addEventListener('click', () => {
+    context_Menu.style.visibility = 'hidden';
 });
